6d4bbd25c6914dacc53d2547cea89cf77bcf53f1
Даниил Сухочев <sushidze@gmail.com>
2022-06-18 23:32:10 +0100

    posts and groups were added

---
+++ yatube/posts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+
+def index(request):
+    title = 'Последние обновления на сайте'
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        'title': title
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    template = 'posts/group_list.html'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
